d0b001f64eaaf9b86b85698bf17d14e131258aa7
Edisson <eacamacho46@misena.edu.co>
2022-01-22 13:19:42 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Hoja de Vida</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <style>
+        #imagen {
+            width: 170px;
+            height: 170px;
+            border-radius: 90px
+        }
+        
+        .pr {
+            background-color: #335384;
+            color: white;
+        }
+        
+        .title {
+            color: #5ba7d1;
+        }
+        
+        .titlepr {
+            color: #335384;
+        }
+    </style>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script src='js/main.js'></script>
+</head>
+
+<body>
+
+    <div class="container font">
+        <div class="row">
+
+            <div class="col-lg-3 pr">
+
+                <br><br><br>
+
+
+                <div class="text-center"><img src="http://imgfz.com/i/7up4QiF.jpeg" id="imagen"></div>
+
+
+                <br><br>
+                <h2>Direccion:</h2>
+                <p> Vereda Cruz de Bonza ,<br> Paipa, Boyacá, Colombia
+                </p>
+
+                <br>
+                <h2>Medios de Contacto:</h2>
+
+                <br>
+                <i class="far fa-envelope-open"></i> eacamacho46@misena.edu.co <br>
+                <i class="fab fa-whatsapp"></i> +57318636150
+
+                <br><br>
+                <h2>Resumen de Habilidades:</h2>
+                <br>
+                <i class="fas fa-rocket"></i> Buena Comunicacion <br>
+                <i class="fas fa-rocket"></i> Manejo de Git <br>
+                <i class="fas fa-rocket"></i> Conocimienos en SQL <br>
+                <i class="fas fa-rocket"></i> Metodologias agiles SCRUM <br>
+                <i class="fas fa-rocket"></i> Mejora de procesos <br>
+                <i class="fas fa-rocket"></i> Capacitación de personal y usuarios
+                <br><br>
+
+                <h2>Reconocimientos Otorgados:</h2>
+
+                <i class="fas fa-graduation-cap"></i> Mejor estudiante ficha 1259025
+
+
+            </div>
+            <div class="col-lg-9">
+                <br><br><br><br>
+
+
+
+
+                <h1 class="titlepr">EDISSON ARMANDO CAMACHO CAMARGO</h1><br>
+                <p>
+                    <h3>TECNOLOGO EN ANALISIS Y DESARROLLO DE SISTEMAS DE INFORMACION</h3>
+                </p>
+                <p>
+
+                </p>
+
+                <br><br>
+                <div class="text-center">
+                    <h4>Barra de Navegacion</h4>
+                </div>
+                <br><br>
+                <div class="card ">
+
+                    <div class="card-body">
+                        <ul class="nav">
+
+                            <li class="nav-item">
+                                <a class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#perfil" aria-controls="perfil">Perfil Ocupacional</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#experiencia" aria-controls="perfil">Experiencia Laboral</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#historial" aria-controls="perfil">Historial Academico</a>
+
+                            </li>
+                            <li class="nav-item">
+
+                                <a href="https://drive.google.com/drive/folders/1Rta4oUTyW6Xgvs6ZaTuuvIpRiPCYlN4i" class="nav-link">Documentos</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+
+
+
+
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="perfil" aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                        <h5 id="offcanvasRightLabel"></h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <h2 class="title ">Perfil Ocupacional:</h2>
+                        <br>
+                        <p>
+                            Especialista en procesos de creación de sistemas de información, con objetivos enfocados en analizar recursos, crear soluciones según especificaciones dadas por el cliente. Con conocimientos en áreas de programación, redes y soporte de aplicaciones según
+                            el mercado laboral. Altas competencias comunicativas en la elaboración de trabajos de equipo.
+                        </p>
+                        <br><br>
+                    </div>
+                </div>
+
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="experiencia" aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                        <h5 id="offcanvasRightLabel"></h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <h2 class="title ">Experiencia Laboral:</h2>
+                        <br>
+                        <br>
+
+
+                        <br>
+                        <h3> Profesional de infraestructura.</h3> <br> Stefanini Sysman | 27 de Noviembre 2017 - 30 de Julio 2020 <br><br><br>
+                        <i class="fas fa-circle "></i> Encargado del soporte tecnico en equipos de computo. <br>
+                        <i class="fas fa-circle "></i> Soporte tecnico a entidad via presencial y remota <br>
+
+                    </div>
+                </div>
+
+
+
+
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="historial" aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                        <h5 id="offcanvasRightLabel"></h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+
+                        <h2 class="title ">Historial Academico:</h2>
+                        <br>
+                        <h3> Servicio Nacional de Aprendizaje</h3>
+                        <br> Tecnico en Sistemas | 22 de Enero 2018 <br><br><br>
+                        <br> <i class="fas fa-circle "></i> Conocimientos informaticos en ofimatica , redes y equipos.
+                        <br> <i class="fas fa-circle "></i> Graduado con honores.
+
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <?php
+
+
+          ?>
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+</body>
+
+</html>
